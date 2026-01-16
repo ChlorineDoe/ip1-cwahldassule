97ddcc1afea896239ad15459e57d74890fbca204
ChlorineDoe <cwahldassule@brandeis.edu>
2026-01-16 05:21:15 +0000

    Majority of dice game complete. Need to fix win display

---
+++ ip1/src/main/java/edu/brandeis/cosi103a/ip1/Player.java
@@ -0,0 +1,31 @@
+package edu.brandeis.cosi103a.ip1;
+
+public class Player {
+    int score;
+    int turns;
+    int maxTurns;
+    String name;
+
+    public Player(int maxTurns, String name) {
+        this.maxTurns = maxTurns;
+        this.score = 0;
+        this.turns = 0;
+        this.name = name;
+    }
+
+    public void addScrore(int points){
+        score += points;
+    }
+
+    public void updateTurn(){
+        turns += 1;
+    }
+
+    public boolean gameOver(){
+        if(turns >= maxTurns){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
